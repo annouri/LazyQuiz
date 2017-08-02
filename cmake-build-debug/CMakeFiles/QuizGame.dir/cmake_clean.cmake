925d801c9d2057da1c76129d83664fb56b5c8d8b
annouri ilias <annouri.ilias@gmail.com>
2017-08-02 13:44:15 +0100

    Code Source Added

---
+++ cmake-build-debug/CMakeFiles/QuizGame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuizGame.dir/main.c.o"
+  "QuizGame.pdb"
+  "QuizGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QuizGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
